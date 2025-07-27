a48e3632c80c46756a13c80649596c2ab3ddf069
ayush srivastava <ayushsrivastavamail@gmail.com>
2025-07-27 14:52:02 +0530

    Fixed development and production

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@
 import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path';
+
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
@@ -9,13 +12,17 @@ import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 const app = express();
-const PORT= process.env.PORT ;
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve(); // Get the current directory name
 
 
-
-app.use(cors({
-  origin: 'http://localhost:5173', // your frontend Vite server
-}));
+// CORS configuration FOR DEVELOPMENT
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: 'http://localhost:5173', // your frontend Vite server
+  }));
+}
 
 //middleware
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
@@ -35,6 +42,18 @@ app.use(rateLimiter); //call our middleware
 
 
 app.use("/api/notes", notesRoutes)
+
+// On production, serve the static files from the frontend build directory
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "dist"))); // Serve static files from the frontend build directory
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // Serve the index
+  })
+}
+// End
+
 
 connectDB()  // Connect to the Database then start the Server
     .then(() => {
